daaf5a34659b94aa2ca4b8e37727f3395b6623fd
alexladines <alexladines@outlook.com>
2019-07-03 09:28:24 -0700

    Create project 1

---
+++ Project1/Project1/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Alex Ladines on 7/3/19.
+//  Copyright © 2019 Alex Ladines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

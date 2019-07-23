460f8fefd0d05111d37d674b1e51d38d91a9a0ed
alexladines <alexladines@outlook.com>
2019-07-22 21:25:29 -0700

    Create project 5

---
+++ Project5/Project5/View Controllers/AnagramsTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  AnagramsTableViewController.swift
+//  Project5
+//
+//  Created by Alex Ladines on 7/22/19.
+//  Copyright © 2019 Alex Ladines. All rights reserved.
+//
+
+import UIKit
+
+class AnagramsTableViewController: UITableViewController {
+    // MARK: - Properties
+
+    // MARK: - IBOutlets
+
+    // MARK: - IBActions
+
+    // MARK: - Life Cycle
+
+    // MARK: - Methods
+
+    // MARK: - Navigation
+
+    // MARK: - UITableViewDataSource
+
+    // MARK: - UITableViewDelegate
+
+}
